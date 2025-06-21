0949a23d93cd9eaa96de64abe754892bdd83c322
Subodh Nandi <subodhnandi59428@gmail.com>
2025-06-21 10:39:52 +0530

    Add files via upload

---
+++ TextBlock.java
@@ -0,0 +1,44 @@
+package com.subodh.StringHandling;
+
+public class TextBlock {
+	public static void main(String[] args) {
+		/*
+		 * write "multiple lines in java use
+		 * 		 String s1="""subodh""";
+		 * 
+		 *
+		 */
+			String s1=
+					"""
+					<head>
+						welcome to html
+					</head>
+					<body>
+						wecome to java program
+					</body>
+					
+					""";
+			String s2=
+					"""
+					   ''
+					""";
+			String s3=
+					"""
+						""
+					""";
+			System.out.println(s1);
+			System.out.println(s2);
+			System.out.println(s3);
+			//..........................
+//			String s4=
+//					"""
+//						"""
+//					""";
+			//String s5="""  """;
+			//String s6="""\n""";
+			
+			
+			
+	}
+
+}
